d38500dd1f6a381969fff5face490e77d1313ebe
Kira Burova <coca-cola-from-hell@yandex.ru>
2019-09-22 21:17:18 +0300

    first problem solved

---
+++ strings/1108.Defanging-an-IP-address/1108.Defanging-an-IP-address.js
@@ -0,0 +1,14 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+/**
+ * @param {string} address
+ * @return {string}
+ */
+const defangIPaddr = function (address) {
+    return address.replace(/\./g, '[.]')
+};
